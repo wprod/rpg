c7f36a741cc9a85706509caf6e9b1f09966d1d81
witters <witters.amand@gmail.com>
2023-06-23 09:40:56 +0200

    feat(Map): add heightMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,35 @@
 import React from "react";
 import ThirdPersonCharacterControls from "./Components/ThirdPersonCharacterControls";
 import { Canvas, useLoader } from "@react-three/fiber";
-import { Debug, Physics, useBox } from "@react-three/cannon";
+import { Debug, Physics } from "@react-three/cannon";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { generateHeightmap, Heightfield } from "./Components/Map/Heightfield";
 
-const PATH = "/models";
+export const MODEL_PATH = "/models";
 
 const animationPaths = {
-  idle: `${PATH}/idle.fbx`,
-  walk: `${PATH}/slowRun.fbx`,
-  run: `${PATH}/fastRun.fbx`,
-  jump: `${PATH}/jumpingUp.fbx`,
-  landing: `${PATH}/jumpingDown.fbx`,
-  inAir: `${PATH}/inAir.fbx`,
-  backpedal: `${PATH}/backpedal.fbx`,
-  turnLeft: `${PATH}/leftTurn.fbx`,
-  turnRight: `${PATH}/rightTurn.fbx`,
-  strafeLeft: `${PATH}/leftStrafe.fbx`,
-  strafeRight: `${PATH}/rightStrafe.fbx`,
+  idle: `${MODEL_PATH}/idle.fbx`,
+  walk: `${MODEL_PATH}/slowRun.fbx`,
+  run: `${MODEL_PATH}/fastRun.fbx`,
+  jump: `${MODEL_PATH}/jumpingUp.fbx`,
+  landing: `${MODEL_PATH}/jumpingDown.fbx`,
+  inAir: `${MODEL_PATH}/inAir.fbx`,
+  backpedal: `${MODEL_PATH}/backpedal.fbx`,
+  turnLeft: `${MODEL_PATH}/leftTurn.fbx`,
+  turnRight: `${MODEL_PATH}/rightTurn.fbx`,
+  strafeLeft: `${MODEL_PATH}/leftStrafe.fbx`,
+  strafeRight: `${MODEL_PATH}/rightStrafe.fbx`,
 };
 
-function Floor() {
-  const [ref] = useBox(() => ({
-    type: "Static",
-    args: [75, 0.2, 75],
-    mass: 0,
-    material: {
-      friction: 0,
-      name: "floor",
-    },
-    collisionFilterGroup: 2,
-  }));
-
-  return (
-    <group>
-      <mesh
-        // @ts-ignore
-        ref={ref}
-      >
-        <boxGeometry name="floor-box" />
-        <meshPhongMaterial opacity={0} transparent />
-      </mesh>
-      <gridHelper args={[25, 25]} />
-    </group>
-  );
-}
-
-function Wall({ args, ...props }: any) {
-  const [ref] = useBox(() => ({
-    type: "Static",
-    args,
-    mass: 0,
-    material: {
-      friction: 0.3,
-      name: "wall",
-    },
-    collisionFilterGroup: 2,
-    ...props,
-  }));
-
-  return (
-    <mesh receiveShadow ref={ref} {...props}>
-      <boxGeometry args={args} />
-      <meshPhongMaterial color="white" opacity={0.8} transparent />
-    </mesh>
-  );
-}
-
 export function App() {
-  const fbx = useLoader(FBXLoader, `/models/model-2.fbx`);
+    const fbx = useLoader(FBXLoader, `/models/model-2.fbx`);
+    const scale = 128;
 
-  const characterProps = {
-    scale: 1,
-    velocity: 8,
-    radius: 0.33,
-  };
+    const characterProps = {
+      scale: 1,
+      velocity: 8,
+      radius: 0.33,
+    };
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -88,21 +43,19 @@ export function App() {
         }}
       >
         <Physics gravity={[0, -35, 0]}>
-          <Debug color="black" scale={1.1}>
-            <Wall args={[50, 3, 0.2]} position={[0, 1.4, -25]} />
-            <Wall args={[50, 3, 0.2]} position={[0, 1.4, 25]} />
-            <Wall args={[2, 2, 2]} position={[0, 0, 0]} />
-            <Wall
-              args={[25, 3, 0.2]}
-              rotation={[0, -Math.PI / 2, 0]}
-              position={[12.6, 1.4, 0]}
+          <Debug color="black" scale={1}>
+            <Heightfield
+              elementSize={scale / 128}
+              heights={generateHeightmap({
+                height: 128,
+                number: 100,
+                scale: 10,
+                width: 128,
+              })}
+              position={[-scale / 2, -10, scale / 2]}
+              rotation={[-Math.PI / 2, 0, 0]}
             />
-            <Wall
-              args={[25, 3, 0.2]}
-              rotation={[0, -Math.PI / 2, 0]}
-              position={[-12.6, 1.4, 0]}
-            />
-            <Floor />
+
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
 
---
+++ src/Components/Map/Floor.tsx
@@ -0,0 +1,28 @@
+import { useBox } from "@react-three/cannon";
+import React from "react";
+
+function Floor() {
+  const [ref] = useBox(() => ({
+    type: "Static",
+    args: [75, 0.2, 75],
+    mass: 0,
+    material: {
+      friction: 0,
+      name: "floor",
+    },
+    collisionFilterGroup: 2,
+  }));
+
+  return (
+    <group>
+      <mesh
+        // @ts-ignore
+        ref={ref}
+      >
+        <boxGeometry name="floor-box" />
+        <meshPhongMaterial opacity={0} transparent />
+      </mesh>
+      <gridHelper args={[25, 25]} />
+    </group>
+  );
+}
